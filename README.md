4a002a247e2abd022c3f500bb70097727ad0e5c0
chrisyue <blizzchris@gmail.com>
2015-10-18 16:05:41 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# busy-eye
-a treatment for lazy-eye
+# Busy Eye
+
+A treatment for lazy-eye
+
+用于弱视治疗
+
+## 使用方式 How to use
+
+请访问此[链接](http://chrisyue.github.io/busy-eye/index.html)
+
+Please follow this [link](http://chrisyue.github.io/busy-eye/index.html)
+
+## 其他 Misc
+
+`busy-eye.html` 只是用于测试的文件
+
+`busy-eye.html` is only for testing
---
+++ busy-eye.js
@@ -0,0 +1,54 @@
+(function() {
+    var mosaicWidth = 100;
+
+    var doc = document;
+
+    var bwidth = doc.documentElement.clientWidth;
+    var bheight = doc.documentElement.clientHeight;
+
+    var nbCols = Math.ceil(bwidth / mosaicWidth);
+    var nbRows = Math.ceil(bheight / mosaicWidth);
+
+    if (0 === nbCols % 2) {
+        nbCols--;
+    }
+
+    nbCols += 2;
+
+    var style = doc.createElement('style');
+    doc.head.appendChild(style);
+    var sheet = style.sheet;
+    sheet.insertRule('* { color: red !important; background: transparent !important }', 0);
+    sheet.insertRule('.busy-eye-mosaic-frame { position: fixed; z-index: -9999; top: 0; bottom: 0; left: 0; right: ' + (bwidth - nbCols * mosaicWidth) + 'px }', 0)
+    sheet.insertRule('.busy-eye-mosaic-frame-translated { transform: translateX(' + (-mosaicWidth) + 'px) }', 0)
+    sheet.insertRule('.busy-eye-mosaic { float: left; width: ' + mosaicWidth + 'px; height: ' + mosaicWidth + 'px }', 0);
+    sheet.insertRule('.busy-eye-mosaic-light { background: #fff !important }', 0);
+    sheet.insertRule('.busy-eye-mosaic-dark { background: #000 !important }', 0);
+
+    var frame = doc.createElement('div');
+    frame.className = 'busy-eye-mosaic-frame';
+
+    doc.body.appendChild(frame);
+
+    var nbMosaic = nbCols * nbRows;
+
+    for (var i = 1; i <= nbMosaic; ++i) {
+        var mosaic = doc.createElement('div');
+
+        if (i % 2) { // odd
+            mosaic.className = 'busy-eye-mosaic busy-eye-mosaic-dark';
+        } else {
+            mosaic.className = 'busy-eye-mosaic busy-eye-mosaic-light';
+        }
+
+        frame.appendChild(mosaic);
+    }
+
+    setInterval(function() {
+        if (frame.className.indexOf('busy-eye-mosaic-frame-translated') >= 0) {
+            frame.className = frame.className.replace(' busy-eye-mosaic-frame-translated', '');
+        } else {
+            frame.className += ' busy-eye-mosaic-frame-translated';
+        }
+    }, 100);
+})();
